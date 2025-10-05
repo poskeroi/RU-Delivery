aa45d8c7e6b77a9ae154f2f5560331ebc6ea3e47
poskeroi <atharvasasankar475@gmail.com>
2025-10-05 11:38:10 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RutgersMap from './components/RutgersMap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RutgersMap />
+    </div>
+  );
+}
+
+export default App;
