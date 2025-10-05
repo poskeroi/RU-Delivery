3d8d1158291daa23261212d40cc5a8242c608adf
poskeroi <atharvasasankar475@gmail.com>
2025-10-05 11:37:25 -0400

    Add files via upload

---
+++ data/MenuItems.js
@@ -0,0 +1,83 @@
+export const menuItems = {
+  collegeAve: {
+    'Atrium (Student Center)': [
+      'Three Chillies Bowl',
+      'Mezze',
+      'Scarlet Ginger',
+      'Kings Hawaiian'
+    ],
+    'Cafe West (Academic Building West)': [
+      'Breakfast Burrito',
+      'Bagel with Cream Cheese',
+      'Coffee',
+      'Muffin',
+      'Yogurt Parfait'
+    ]
+  },
+  busch: {
+    'Busch Dining Takeout': [
+      'Burger',
+      'Pizza Slice',
+      'Chicken Tenders',
+      'Garden Salad',
+      'Fries'
+    ],
+    'Busch Harvest': [
+      'Stir Fry Bowl',
+      'Rice Bowl',
+      'Veggie Wrap',
+      'Fruit Cup'
+    ],
+    "Woody's Cafe": [
+      'Hot Dog',
+      'Grilled Cheese',
+      'Chips',
+      'Soda',
+      'Cookie'
+    ]
+  },
+  livingston: {
+    'Kilmers': [
+      'Chicken Wings',
+      'Mozzarella Sticks',
+      'Onion Rings',
+      'Loaded Fries',
+      'Samosas',
+      'Chicken Nuggets',
+      'Soda'
+    ],
+    'Sbarros (Livi Student Center)': [
+      'Pepperoni Pizza',
+      'Chicken Barbeque Pizza',
+      'Stromboli',
+      'Philly Cheesesteak',
+      'Chicken Wings',
+      'Chicken 4 Cheese'
+    ],
+    "Henry's Diner": [
+      'Wraps',
+      'Breakfast Menu',
+      'Sandwich',
+      'Cake',
+      'Milkshake',
+      'Burger',
+      'Fries'
+    ]
+  },
+  cookDoug: {
+    'Harvest': [
+      'Buddha Bowl',
+      'Quinoa Salad',
+      'Smoothie',
+      'Veggie Burger',
+      'Hummus Wrap'
+    ],
+    'Cook Cafe': [
+      'Sandwich',
+      'Soup of the Day',
+      'Side Salad',
+      'Chips',
+      'Beverage'
+    ]
+  }
+};
